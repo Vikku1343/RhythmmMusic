b199846795cefcc2503ecd7f2ae64ce1b0c55402
ᴠɪᴋᴀꜱʜ ɢᴜᴘᴛᴛᴀ <102718297+Vikku1343@users.noreply.github.com>
2023-04-06 03:36:14 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/491d40f7a9f2062cbac85.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/da87b269ac7e7eee24bcd.jpg")
+
+SESSION = getenv("SESSION", None)
+
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RHYTHMM_SUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ISHQSUFIIYANA")
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+
+
+FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
